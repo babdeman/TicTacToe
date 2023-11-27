f6374ff697ee42ef51b166da6c8a26dd9b67b272
babdeman <80133701+babdeman@users.noreply.github.com>
2023-11-27 17:01:10 +0100

    finished v0.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
 public class Main {
+    public static void test0(Game game) throws PlayerWon {;
+        game.startGame();
+        game.placeMarker(0, 0);
+        game.placeMarker(0, 1);
+        game.placeMarker(1, 0);
+        game.placeMarker(1, 1);
+        game.placeMarker(2, 0);
+        game.placeMarker(2, 1);
+    }
+    public static void test1(Game game) throws PlayerWon {
+        game.startGame();
+        game.placeMarker(0, 1);
+        game.placeMarker(0, 0);
+        game.placeMarker(1, 2);
+        game.placeMarker(1, 1);
+        game.placeMarker(2, 0);
+        game.placeMarker(2, 2);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game game = new Game();
+        try{
+            test1(game);
+        }
+        catch(PlayerWon playerWon){
+            System.out.println(playerWon.getPlayer() + " won");
+        }
+        finally{
+            System.out.println(game);
+        }
     }
 }
